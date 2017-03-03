bf0408acdc9e7d3f8b1dc4cd2cfeb877b8d6f718
NORDBY\Mathias <Mathias_nordby@hotmail.com>
2017-03-03 12:28:09 +0100

    Added custom enemy

---
+++ Assets/scripts/EnemyController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : EnemyScript {
+
+
+
+	// Use this for initialization
+	void Start () {
+        base.Start();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        base.Update();
+	}
+
+    public void OnTriggerEnter2D(Collider2D coll)
+    {
+        base.OnTriggerEnter2D(coll);
+    }
+
+}
